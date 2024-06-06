7f531928ce7feadde227cd1b53be2c01648dbe06
Fran3103 <fn.aguirre@hotmail.com>
2024-06-06 01:10:47 -0300

    todo a back

--- src/main/java/deportivos/deportivosgroup/Repositories/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package deportivos.deportivosgroup.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import deportivos.deportivosgroup.Entities.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long>{
-    boolean existsByTitulo( String titulo);
-}
