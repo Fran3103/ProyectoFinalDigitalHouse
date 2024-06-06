7d789a05c5580c5e90c8dc21ac8c0390f89b6758
Fran3103 <fn.aguirre@hotmail.com>
2024-06-06 00:54:42 -0300

    arreglando actualizacion

--- src/main/java/deportivos/deportivosgroup/Repositories/ProductoRepository.java
+++ src/main/java/deportivos/deportivosgroup/Repositories/ProductoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import deportivos.deportivosgroup.Entities.Producto;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
-    boolean tituloRepetido( String titulo);
+    boolean existsByTitulo( String titulo);
 }
